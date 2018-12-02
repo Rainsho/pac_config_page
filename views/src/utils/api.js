67da31af79ebe93a58563dc9f7fdcbb4db1bd8e0
Rainsho <djkyux@163.com>
2018-12-02 15:05:38 +0800

    front-end api

--- views/src/utils/api.js
+++ views/src/utils/api.js
@@ -2,24 +2,35 @@ import { Request } from './util';
 import config from './config';
 
 const { SERVER } = config;
-const FS_API = `${SERVER}fs/file`;
 
 export function getDisk() {
   return Request.get(`${SERVER}fs/disk`);
 }
 
 export function getFiles() {
-  return Request.get(FS_API);
+  return Request.get(`${SERVER}fs/file`);
 }
 
 export function renameFile(path = '', name) {
-  return Request.put(FS_API, { path, name });
+  return Request.put(`${SERVER}fs/file`, { path, name });
 }
 
 export function deleteFile(path, purge) {
-  return Request.put(FS_API, { path, purge });
+  return Request.put(`${SERVER}fs/file`, { path, purge });
 }
 
 export function getAddress() {
   return Request.get(`${SERVER}pac/address`);
+}
+
+export function updateAddress() {
+  return Request.put(`${SERVER}pac/address`);
+}
+
+export function getPing() {
+  return Request.get(`${SERVER}/pac/ping`);
+}
+
+export function updateConfig(cur, min) {
+  return Request.put(`${SERVER}/pac/config`, { cur, min });
 }
