c8e7dc6bf63b71c5197af57f90fd21d9138a9c36
Rainsho <djkyux@163.com>
2020-08-15 23:48:39 +0800

    update UI

--- views/src/components/common/PingInfo.js
+++ views/src/components/common/PingInfo.js
@@ -7,22 +7,22 @@ const PingInfo = ({ current, ps, pingInfo, handleSwitch }) => {
 
   return (
     <Row type="flex" style={{ minHeight: 30 }}>
-      <Col xs={6} sm={4} xl={3}>
+      <Col xs={6} sm={4} md={3}>
         {current === ps ? (
           <Switch checked />
         ) : (
           <Switch checked={false} onClick={() => handleSwitch(ps)} />
         )}
       </Col>
-      <Col xs={16} sm={8} xl={6}>
+      <Col xs={18} sm={8} md={12} lg={10} xl={6}>
         <Color.WhatBlue str="host" />
         {ps}
       </Col>
       {pingInfo && (
-        <Col xs={{ offset: 6, span: 20 }} sm={{ offset: 0, span: 8 }} xl={6}>
+        <Col xs={{ offset: 6, span: 20 }} sm={{ offset: 0, span: 8 }} md={6}>
           {alive ? (
             <>
-              <Color.LivingCoral str="avg_ping" />
+              <Color.LivingCoral str="ping" />
               {avg} ms
             </>
           ) : (
